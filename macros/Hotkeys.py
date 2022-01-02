48d095b67ee51cc8be0bafbfb2586ec5f84df7b6
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-01-02 01:52:17 -0800

    Update Hotkeys.py

--- macros/Hotkeys.py
+++ macros/Hotkeys.py
@@ -1,15 +1,5 @@
-# MACROPAD Hotkeys example: Consumer Control codes (media keys)
+# MACROPAD Hotkeys example: Hotkeys
 
-# The syntax for Consumer Control macros is a little peculiar, in order to
-# maintain backward compatibility with the original keycode-only macro files.
-# The third item for each macro is a list in brackets, and each value within
-# is normally an integer (Keycode), float (delay) or string (typed literally).
-# Consumer Control codes are distinguished by enclosing them in a list within
-# the list, which is why you'll see double brackets [[ ]] below.
-# Like Keycodes, Consumer Control codes can be positive (press) or negative
-# (release), and float values can be inserted for pauses.
-
-# To reference Consumer Control codes, import ConsumerControlCode like so...
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keycode import Keycode
 # You can still import Keycode as well if a macro file mixes types!
