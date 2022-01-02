6d393c4895c87e03269e0633891232fdfadba045
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-01-01 22:36:48 -0800

    Create Blank.py

---
+++ macros/Blank.py
@@ -0,0 +1,41 @@
+# MACROPAD Hotkeys example: Consumer Control codes (media keys)
+
+# The syntax for Consumer Control macros is a little peculiar, in order to
+# maintain backward compatibility with the original keycode-only macro files.
+# The third item for each macro is a list in brackets, and each value within
+# is normally an integer (Keycode), float (delay) or string (typed literally).
+# Consumer Control codes are distinguished by enclosing them in a list within
+# the list, which is why you'll see double brackets [[ ]] below.
+# Like Keycodes, Consumer Control codes can be positive (press) or negative
+# (release), and float values can be inserted for pauses.
+
+# To reference Consumer Control codes, import ConsumerControlCode like so...
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from adafruit_hid.keycode import Keycode
+# You can still import Keycode as well if a macro file mixes types!
+# See other macro files for typical Keycode examples.
+
+app = {               # REQUIRED dict, must be named 'app'
+    'name' : '', # Application name
+    'macros' : [      # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x000000, '', []),
+        (0x000000, '', []),
+        (0x000000, '', []),
+        # 2nd row ----------
+        (0x000000, '', []),
+        (0x000000, '', []),
+        (0x000000, '', []),
+        # 3rd row ----------
+        (0x000000, '', []),
+        (0x000000, '', []),
+        (0x000000, '', []),
+        # 4th row ----------
+        (0x000000, '', []),
+        (0x000000, '', []),
+        (0x000000, '', []),
+        # Encoder button ---
+        (0x000000, '', [])
+    ]
+}
