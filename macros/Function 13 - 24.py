4c3a57b1026c50883585f7c8e2c5b43806720467
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-01-04 21:17:39 -0800

    Update Function 13 - 24.py

--- macros/Function 13 - 24.py
+++ macros/Function 13 - 24.py
@@ -7,7 +7,7 @@ from adafruit_hid.keycode import Keycode
 # See other macro files for typical Keycode examples.
 
 app = {               # REQUIRED dict, must be named 'app'
-    'name' : 'Fuction Keys 2', # Application name
+    'name' : 'Function Keys 2', # Application name
     'macros' : [      # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
