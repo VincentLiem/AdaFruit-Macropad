ebc8b0a496a28c2cbc73ed948d3c516bd5b2be83
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-02-02 12:33:51 -0800

    Update Function 1- 12.py

--- macros/Function 1- 12.py
+++ macros/Function 1- 12.py
@@ -2,8 +2,7 @@
 
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keycode import Keycode
-# You can still import Keycode as well if a macro file mixes types!
-# See other macro files for typical Keycode examples.
+
 
 app = {               # REQUIRED dict, must be named 'app'
     'name' : 'Function Keys', # Application name
