2b92befc584878abee551a26c8ccb4ce726ba775
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-02-02 12:34:37 -0800

    Update Numpad.py

--- macros/Numpad.py
+++ macros/Numpad.py
@@ -2,6 +2,7 @@
 
 from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
 
+
 app = {                # REQUIRED dict, must be named 'app'
     'name' : 'Numpad', # Application name
     'macros' : [       # List of button macros...
