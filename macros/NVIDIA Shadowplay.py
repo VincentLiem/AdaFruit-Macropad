6588a47f58653250753fa041e92fe36967831750
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-21 16:52:30 -0700

    Update NVIDIA Shadowplay.py

--- macros/NVIDIA Shadowplay.py
+++ macros/NVIDIA Shadowplay.py
@@ -9,15 +9,15 @@ app = {               # REQUIRED dict, must be named 'app'
         # 1st row ----------
         (0x000000, 'Overlay', [Keycode.ALT, 'z']), #Shadowplay Overlay
         (0x000000, 'Pic', [Keycode.ALT, Keycode.F1]), #Screenshot
-        (0x000000, 'Video', [Keycode.ALT, Keycode.F10]), #Save Instant Replay
+        (0x000000, 'Rec', [Keycode.ALT, Keycode.F10]), #Save Instant Replay
         # 2nd row ----------
         (0x000000, 'Stats', [Keycode.CONTROL, Keycode.ALT, 'r']), #Performance Overlay
         (0x000000, 'Mic', [Keycode.CONTROL, Keycode.ALT, 'm']), # Toggle Mic````
         (0x000000, 'P2T', ['`']), #Push to Talk
         # 3rd row ----------
-        (0x000000, 'LIVE on/off', [Keycode.ALT, Keycode.F8]), #Toggle LIVE
-        (0x000000, 'Live Pause', [Keycode.ALT, Keycode.F7]), #Pause/Resume LIVE
-        (0x000000, 'Live Video', [Keycode.ALT, Keycode.F6]), #LIVE Video on/off
+        (0x000000, 'LIVE on', [Keycode.ALT, Keycode.F8]), #Toggle LIVE
+        (0x000000, 'Pause', [Keycode.ALT, Keycode.F7]), #Pause/Resume LIVE
+        (0x000000, 'Video', [Keycode.ALT, Keycode.F6]), #LIVE Video on/off
         # 4th row ----------
         (0x000000, 'Vol -', [[ConsumerControlCode.VOLUME_DECREMENT]]),
         (0x000000, 'Mute', [[ConsumerControlCode.MUTE]]),
